550fc32dd8dfb4c5b5df6f08e706ff4431740bf8
black9 <black9@MacBook-Pro-black9.local>
2015-01-25 12:23:05 +0200

    Initial Commit

---
+++ VKStatuses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKStatuses
+//
+//  Created by black9 on 25/01/15.
+//  Copyright (c) 2015 black9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKStatuses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKStatuses
+//
+//  Created by black9 on 25/01/15.
+//  Copyright (c) 2015 black9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
